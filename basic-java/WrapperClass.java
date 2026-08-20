d4feb530025fbbe95ee2c4292ce9f0f6fe67e79e
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-21 01:30:16 +0530

    wrapper class

---
+++ basic-java/WrapperClass.java
@@ -0,0 +1,16 @@
+/**
+ * WrapperClass
+ */
+public class WrapperClass {
+
+    public static void main(String[] args) {
+        
+
+      int num = 20;
+      Integer number = num;    // auto- boxing
+
+
+      int num2  = number ;   // auto - unboxing
+    }
+}
+
