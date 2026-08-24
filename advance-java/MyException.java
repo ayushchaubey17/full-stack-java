9813ba6b12a6151d5e51fdc9548535e4275ae806
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-24 05:56:08 +0530

    nested catch

--- advance-java/MyException.java
+++ advance-java/MyException.java
@@ -14,11 +14,17 @@ public class MyException {
 try {
     System.out.println(a/b);
     
-} catch (Exception e) {
+} catch (ArrayIndexOutOfBoundsException e) {
     System.out.println(e);
     // TODO: handle exception
 }
+catch(ArithmeticException e){
+    System.out.println("its arithmetic exception");
+}
 
+catch(Exception e){
+    System.out.println("unknown exception");
+}
 
         System.out.println( "jitdf");
     }
