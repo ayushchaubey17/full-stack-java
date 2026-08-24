5ddf045e1c52f394c7f1dfecb868a350a5c2607a
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-24 06:01:48 +0530

    with throws

--- advance-java/MyException.java
+++ advance-java/MyException.java
@@ -5,27 +5,31 @@
 public class MyException {
 
     public static void main(String[] args) {
-        
 
         int a = 20;
         int b = 0;
 
+        try {
+            if (b==0) {
+                
+            throw new ArithmeticException("can not able to divide");
+            // throw new AyushException("df");
+            }
+            System.out.println(a / b);
 
-try {
-    System.out.println(a/b);
-    
-} catch (ArrayIndexOutOfBoundsException e) {
-    System.out.println(e);
-    // TODO: handle exception
-}
-catch(ArithmeticException e){
-    System.out.println("its arithmetic exception");
-}
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+            // TODO: handle exception
+        } catch (ArithmeticException e) {
+            System.out.println("its arithmetic exception");
+        }
 
-catch(Exception e){
-    System.out.println("unknown exception");
-}
+        catch (Exception e) {
+            System.out.println("unknown exception");
+        }
 
-        System.out.println( "jitdf");
+        System.out.println("jitdf");
     }
 }
+
+
