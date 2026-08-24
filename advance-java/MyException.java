0cfefdce3484f432c8c15b9fa4a08084e33ec7d8
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-24 05:53:57 +0530

    exceptions in java

---
+++ advance-java/MyException.java
@@ -0,0 +1,25 @@
+
+/**
+ * MyException
+ */
+public class MyException {
+
+    public static void main(String[] args) {
+        
+
+        int a = 20;
+        int b = 0;
+
+
+try {
+    System.out.println(a/b);
+    
+} catch (Exception e) {
+    System.out.println(e);
+    // TODO: handle exception
+}
+
+
+        System.out.println( "jitdf");
+    }
+}
