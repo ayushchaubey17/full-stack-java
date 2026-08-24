896a16d11106ef8ff8b0bd5359bff476ab0bf8fd
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-24 06:28:59 +0530

    using system.in.read

--- advance-java/BufferReaderAndScanner.java
+++ advance-java/BufferReaderAndScanner.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+
 /**
  * BufferReaderAndScanner
  */
 public class BufferReaderAndScanner {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("hyyy");
+
+        int n = System.in.read();
+        System.out.println(n-48);
+
+
+    }
     
 }
