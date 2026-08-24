b409c35385a0af5bd06976ae6673345eefaa5b73
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-24 08:29:23 +0530

    thread priority and sleep

--- advance-java/MyThread.java
+++ advance-java/MyThread.java
@@ -9,6 +9,12 @@ public class MyThread {
 
         A a = new A();
         B b = new B();
+
+        a.setPriority(10);
+
+        System.out.println(a.getPriority());  //5 default
+        System.out.println(b.getPriority());  //5 by
+
         a.start();
         b.start();
     }
@@ -18,6 +24,12 @@ class A  extends Thread{
     public void run() {
         for (int i = 0; i < 20; i++) {
             System.out.println("a");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -26,6 +38,12 @@ class B extends Thread{
     public void run() {
         for (int i = 0; i < 20; i++) {
             System.out.println("b");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
