bdb4fdc5a5e9dc63fb9bfb44f43de779245e7be1
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-24 06:33:41 +0530

    using buffer reader

--- advance-java/BufferReaderAndScanner.java
+++ advance-java/BufferReaderAndScanner.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * BufferReaderAndScanner
@@ -8,9 +10,15 @@ public class BufferReaderAndScanner {
     public static void main(String[] args) throws IOException {
         System.out.println("hyyy");
 
-        int n = System.in.read();
-        System.out.println(n-48);
+        // int n = System.in.read();
+        // System.out.println(n-48);
 
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+
+   String str =      br.readLine();
+   System.out.println(str);
 
     }
     
