722630de31d9ed542d7d068700d383fd2dc0da75
ayushchaubey17 <ayush96361570@gmail.com>
2026-08-27 09:16:23 +0530

    comparator in java for String

---
+++ advance-java/ComparingString.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * ComparingString
+ */
+public class ComparingString {
+
+    public static void main(String[] args) {
+
+
+        Comparator<String> comparator = new Comparator<>() {
+            
+            @Override
+            public int compare(String s1, String s2){
+                return s1.length()>s2.length()?1:-1;
+
+            }
+        };
+        List <String> list = new ArrayList<>();
+        list.add("ayush");
+        list.add("bysh");
+        list.add("cyush");
+        list.add("aysfsfssh");
+        list.add("myush");
+
+
+
+        System.out.println(list);
+
+
+        Collections.sort(list,comparator);
+
+        System.out.println(list);
+    }
+}
